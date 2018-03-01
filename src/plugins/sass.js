ae31b31018a1d98ce1108cc77ade1600d1c4e005
Juan Diego Araya <juand.95820@gmail.com>
2018-02-28 23:12:57 -0600

    Build and preview done, missing sass livereload

--- src/plugins/sass.js
+++ src/plugins/sass.js
@@ -3,13 +3,13 @@ const { parse } = require('path');
 
 function compile(file, filename) {
   const fName = parse(filename);
-  if (fName.ext === 'scss') {
-    if (fName.name.slice(0, 2) === '__') {
-      return '';
+  if (fName.ext === '.scss') {
+    if (fName.name.slice(0, 1) !== '_') {
+      return nodeSass.renderSync({ data: file, includePaths: [`${process.cwd()}/src/style/`] })
+        .css;
     }
-  } else {
-    return nodeSass.renderSync({ data: file }).css;
   }
+  return file;
 }
 
 module.exports.callback = (file, filename) => compile(file, filename);
